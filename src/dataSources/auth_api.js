65126cfc6a5d8d27fd1c864b11fea291b2b5278d
Ronrook <78703403+Ronrook@users.noreply.github.com>
2021-12-06 14:27:42 -0500

    Finalizacion DataSource de usuarios

--- src/dataSources/auth_api.js
+++ src/dataSources/auth_api.js
@@ -1,8 +1,9 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
 
+// Importacion de las urls de los microservicios
 const serverConfig = require('../server');
 
-
+// clase DataSource de usuarios 
 class AuthAPI extends RESTDataSource {
 
     constructor() {
@@ -10,20 +11,24 @@ class AuthAPI extends RESTDataSource {
         this.baseURL = serverConfig.auth_api_url;
     }
 
+    // metodo que se comunica con el mircoservico usurios y por medio de un POST se crea usuario  
     async createUser(user) {
         user = new Object(JSON.parse(JSON.stringify(user)));
         return await this.post(`/user/`, user);
     }
 
+    // metodo GET para recuperar un usurio por medio del id
     async getUser(userId) {
         return await this.get(`/user/${userId}/`);
     }
-
+    
+    // metodo que permite hacer login de usurio 
     async authRequest(credentials) {
         credentials = new Object(JSON.parse(JSON.stringify(credentials)));
         return await this.post(`/login/`, credentials);
     }
-
+    
+    // metodo POST para actulizar un token access con un token refresh
     async refreshToken(token) {
         token = new Object(JSON.parse(JSON.stringify({ refresh: token })));
         return await this.post(`/refresh/`, token);
